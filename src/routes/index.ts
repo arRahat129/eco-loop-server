f78dc97a37b67dd32028369923d56eeefe1480c9
arRahat129 <rahashik129@gmail.com>
2026-08-10 18:22:14 +0600

    feat: aggregate all module routers in routes/index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,14 @@
 import { Router } from "express";
+import users from "../services/users";
+import categories from "../services/categories";
+import products from "../services/products";
+import reviews from "../services/reviews";
 
 const router = Router();
+
+router.use("/users", users);
+router.use("/categories", categories);
+router.use("/products", products);
+router.use("/reviews", reviews);
 
 export default router;
